a6c5be4eb7204f20fb10c1a7627c0b44e70a0c88
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-09 16:24:41 -0700

    troubleshooting authorization

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -43,7 +43,7 @@ namespace Library
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
-
+      app.UseAuthentication();
       app.UseSwagger();
       app.UseSwaggerUI(c =>
       {
